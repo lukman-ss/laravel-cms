f0925d7dbc56abf0d267274b0442a25fb810bf70
lukman-ss <64991223+lukman-ss@users.noreply.github.com>
2023-11-16 19:46:27 +0700

    finishing sidebar

--- resources/views/livewire/partials/sidebar.blade.php
+++ resources/views/livewire/partials/sidebar.blade.php
@@ -1,5 +1,8 @@
 <!--begin::Aside-->
-<div id="kt_aside" class="aside py-9" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_toggle">
+<div id="kt_aside" class="aside py-9" data-kt-drawer="true" data-kt-drawer-name="aside"
+    data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true"
+    data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start"
+    data-kt-drawer-toggle="#kt_aside_toggle">
     <!--begin::Brand-->
     <div class="aside-logo flex-column-auto px-9 mb-9" id="kt_aside_logo">
         <!--begin::Logo-->
@@ -13,50 +16,61 @@
     <!--begin::Aside menu-->
     <div class="aside-menu flex-column-fluid ps-5 pe-3 mb-9" id="kt_aside_menu">
         <!--begin::Aside Menu-->
-        <div class="w-100 hover-scroll-overlay-y d-flex pe-3" id="kt_aside_menu_wrapper" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer" data-kt-scroll-wrappers="#kt_aside, #kt_aside_menu, #kt_aside_menu_wrapper" data-kt-scroll-offset="100">
+        <div class="w-100 hover-scroll-overlay-y d-flex pe-3" id="kt_aside_menu_wrapper" data-kt-scroll="true"
+            data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-height="auto"
+            data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer"
+            data-kt-scroll-wrappers="#kt_aside, #kt_aside_menu, #kt_aside_menu_wrapper" data-kt-scroll-offset="100">
             <!--begin::Menu-->
-            <div class="menu menu-column menu-rounded menu-sub-indention menu-active-bg fw-semibold my-auto" id="#kt_aside_menu" data-kt-menu="true">
+            <div class="menu menu-column menu-rounded menu-sub-indention menu-active-bg fw-semibold my-auto"
+                id="#kt_aside_menu" data-kt-menu="true">
+
+                @foreach ($links as $link)
                 <!--begin:Menu item-->
-                <div data-kt-menu-trigger="click" class="menu-item here show menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item @if($link['main_link'] === $active_main_link) here show @endif menu-accordion">
                     <!--begin:Menu link-->
+                    @if(isset($link['sub_link']))
                     <span class="menu-link">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-black-right fs-2"></i>
                         </span>
-                        <span class="menu-title">Dashboards</span>
-                    </span>
-                    <!--end:Menu link-->
-                </div>
-                <!--end:Menu item-->
-                <!--begin:Menu item-->
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-                    <!--begin:Menu link-->
-                    <span class="menu-link">
-                        <span class="menu-icon">
-                            <i class="ki-duotone ki-black-right fs-2"></i>
-                        </span>
-                        <span class="menu-title">Help</span>
+                        <span class="menu-title">{{ $link['main_link'] }}</span>
                         <span class="menu-arrow"></span>
                     </span>
                     <!--end:Menu link-->
+                    @else
+                    <a href="{{ url($link['main_link']) }}">
+                        <span class="menu-link">
+                            <span class="menu-icon">
+                                <i class="ki-duotone ki-black-right fs-2"></i>
+                            </span>
+                            <span class="menu-title">{{ $link['main_link'] }}</span>
+                            <span class="menu-arrow"></span>
+                        </span>
+                    </a>
+                    @endif
+                    @if(isset($link['sub_link']))
+                    @foreach ($link['sub_link'] as $sub_link)
                     <!--begin:Menu sub-->
                     <div class="menu-sub menu-sub-accordion">
                         <!--begin:Menu item-->
                         <div class="menu-item">
                             <!--begin:Menu link-->
-                            <a class="menu-link" href="/" >
+                            <a class="menu-link @if($sub_link === $active_sub_link) active @endif" href="{{ url($link['main_link'] . '/' . $sub_link) }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
-                                <span class="menu-title">Components</span>
+                                <span class="menu-title">{{ $sub_link }}</span>
                             </a>
                             <!--end:Menu link-->
                         </div>
                         <!--end:Menu item-->
                     </div>
                     <!--end:Menu sub-->
+                    @endforeach
+                    @endif
                 </div>
                 <!--end:Menu item-->
+                @endforeach
             </div>
             <!--end::Menu-->
         </div>
@@ -79,23 +93,23 @@
                     <!--begin::Name-->
                     <a href="#" class="text-gray-800 text-hover-primary fs-6 fw-bold lh-1">Paul Melone</a>
                     <!--end::Name-->
-                    <!--begin::Major-->
-                    <span class="text-muted fw-semibold d-block fs-7 lh-1">Python Dev</span>
-                    <!--end::Major-->
                 </div>
                 <!--end::User info-->
             </div>
             <!--end::Wrapper-->
             <!--begin::User menu-->
             <div class="ms-1">
-                <div class="btn btn-sm btn-icon btn-active-color-primary position-relative me-n2" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-overflow="true" data-kt-menu-placement="top-end">
+                <div class="btn btn-sm btn-icon btn-active-color-primary position-relative me-n2"
+                    data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-overflow="true"
+                    data-kt-menu-placement="top-end">
                     <i class="ki-duotone ki-setting-2 fs-1">
                         <span class="path1"></span>
                         <span class="path2"></span>
                     </i>
                 </div>
                 <!--begin::User account menu-->
-                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
+                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px"
+                    data-kt-menu="true">
                     <!--begin::Menu item-->
                     <div class="menu-item px-3">
                         <div class="menu-content d-flex align-items-center px-3">
@@ -107,7 +121,7 @@
                             <!--begin::Username-->
                             <div class="d-flex flex-column">
                                 <div class="fw-bold d-flex align-items-center fs-5">Max Smith
-                                <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span></div>
+                                    <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span></div>
                                 <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">max@kt.com</a>
                             </div>
                             <!--end::Username-->
@@ -122,12 +136,13 @@
                         <a href="../../demo3/dist/account/settings.html" class="menu-link px-5">Account Settings</a>
                     </div>
                     <!--end::Menu item-->
-                     <!--begin::Menu separator-->
-                     <div class="separator my-2"></div>
-                     <!--end::Menu separator-->
+                    <!--begin::Menu separator-->
+                    <div class="separator my-2"></div>
+                    <!--end::Menu separator-->
                     <!--begin::Menu item-->
                     <div class="menu-item px-5">
-                        <a href="../../demo3/dist/authentication/layouts/corporate/sign-in.html" class="menu-link px-5">Sign Out</a>
+                        <a href="../../demo3/dist/authentication/layouts/corporate/sign-in.html"
+                            class="menu-link px-5">Sign Out</a>
                     </div>
                     <!--end::Menu item-->
                 </div>
